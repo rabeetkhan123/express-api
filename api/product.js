506933eec5083416e47cfee91ec4185c3a528739
rabeetkhan123 <rabeetkhan2005@gmail.com>
2023-08-17 19:52:26 +0500

    post routes added

--- api/product.js
+++ api/product.js
@@ -12,4 +12,45 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.post('/register', (req, res) => {
+    const { name, email, password, age } = req.body
+    if (!name || !email || !password || !age) {
+        return res.status(422).json({ error: "fill completely" })
+    }
+
+    User.findOne({ email: email }).then((userExist) => {
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exist" })
+        }
+        const user = new User({ name, email, password, age })
+        user.save().then(() => {
+            res.status(201).json({ message: "User successfully registered" })
+        }).catch(() => {
+            res.status(500).json({ error: "Failed to register" })
+        })
+    }).catch(err => { console.log(err) })
+})
+
+router.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(422).json({ error: "fill completely" })
+        }
+        const userLogin = await User.findOne({ email: email })
+        if (userLogin) {
+            if (userLogin.password == password) {
+                res.json({ message: "User Signin Successfully" })
+            } else {
+                res.json({ message: "Incorrect password" })
+            }
+        } else {
+            res.status(422).json({ error: "Email not registered" })
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const product = require('./api/product')
 const mongoose = require('mongoose');
 
 const app = express()
 const uri = "mongodb+srv://rabeetkhan2005:test123@cluster0.rcfz529.mongodb.net/backend_db?retryWrites=true&w=majority"
+
+app.use(express.json());
+const product = require('./api/product')
 
 mongoose.connect(uri).then(() => {
     console.log("Connected to MongoDB")
@@ -14,3 +16,4 @@ const PORT = process.env.PORT || 5050
 app.use("/api/product", product)
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+
