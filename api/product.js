f97284812f17553e4524da7a4ae89f517fdb249d
rabeetkhan123 <rabeetkhan2005@gmail.com>
2023-08-17 19:23:01 +0500

    my code added

--- api/product.js
+++ api/product.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/userSchema')
 
 router.get("/", async (req, res) => {
     try {
-        res.json({
-            status: 200,
-            message: "Get data has successful"
-        })
+        let data = await User.find({})
+        res.send({ message: data })
     } catch (error) {
         console.log(error)
         return res.status(500).send("Server error")
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const product = require('./api/product')
+const mongoose = require('mongoose');
 
 const app = express()
+const uri = "mongodb+srv://rabeetkhan2005:test123@cluster0.rcfz529.mongodb.net/backend_db?retryWrites=true&w=majority"
+
+mongoose.connect(uri).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => console.log("Error in connection"))
 
 const PORT = process.env.PORT || 5050
 
